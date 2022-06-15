76857fc59504cc7d95cbff1846d0c3125a2e4021
marpuriganesh <marpuriganesh@gmail.com>
2022-06-15 08:32:30 +0530

    added some stuff

---
+++ test json.py
@@ -0,0 +1,14 @@
+
+from cgi import test
+import pandas as pd
+
+dic = {
+    1:[1,2,3,4,5],
+    2:['a','b','c','d','e']
+}
+
+print(dic)
+test = pd.DataFrame(dic)
+print(test)
+
+test.to_json("test.json")
